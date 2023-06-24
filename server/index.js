107e3c2a806af868d9b305147abb2751c0d32312
HoViet0107 <hoquocviet.na.da@gmail.com>
2023-06-24 09:38:58 +0700

    init socialpedia project

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import env from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+
+// học tiếp ở đây
+// lý do: học frontend truóc nên bỏ ngang
+// https://www.youtube.com/watch?v=K8YELRmUb5o&list=PLe4R7LwFw5r6aamFUcA9ZifmErMoEBbjO&index=17
